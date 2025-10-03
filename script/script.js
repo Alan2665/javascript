e2db0a7f010a95e9e934b864071e57e9d4d5117a
Alan26 <arguelloalan@gmail.com>
2025-10-03 11:06:29 -0500

    avance condiciones

--- script/script.js
+++ script/script.js
@@ -39,4 +39,59 @@ function mostrarNombre() {
 }
 //manera de llamar a mi funcion
 mostrarNombre();
-document.writeln("<br> nombre"+nombre);
+document.writeln("<br> nombre" + nombre);
+//funcion que recibe parametros
+function sumar(a, b) {
+    let resultado;
+    resultado = a + b;
+    return resultado;
+}
+//llamamos a la funcion sumar
+var suma = sumar(40, 50);
+document.writeln("<br>la suma es:" + suma);
+/*operadoes aritmeticos:
++:suma
+-:resta
+*:multiplicacion
+/:division
+%:modulo(resuido de una division)
+++:incremento
+--:Devremento
+aoperadores de asignacion:
+estos operadores se utilizan para asignar valores a las 
+variables.
+=:Asignacion simple
++=:asignacion con suma (y+= es equivalente a y=y+b)
+-=:asignacion con resta (y-= es equivalente a y=y-b)
+===:Igualdad estricta(compara valor y tipo)
+operadores de comparacion:
+estos operadores se utilizan para comparar dos valores u devuelven un valor boolean
+==:igualdad
+!=:desigualdad
+>:mayor que 
+<:menor que
+>=:mayor o igual que
+<=menor o igual que
+operadores logicos:
+estas operaciones sirven para combinar expresiones bollenas
+&&:AND devuelve true si ambas condiciones son verdaderas
+||:OR devuelve true si al menos una de las condiciones es verdadera.
+¡:NOT invierte el valor de una condicion
+
+La condicion nos permite ejecutar un bloque de codigo si se 
+cumple una condicion
+sintaxis:
+if(condicion){
+//bloque de codigo a ejecutar si la condicion es verdadera
+}else{
+    //bloque de codigo a ejecutar si la condicion es falsa
+}
+*/
+//crear un script donde se verifique la edad mayor de una perosona 
+var edadElvis=40;
+var edadJustin=40;
+if (edadElvis>edadJustin){
+    document.writeln("<br> Elvis es mayor que Justin");
+}else{
+    document.writeln("<br> Justin es mayor que Elvis")
+}
